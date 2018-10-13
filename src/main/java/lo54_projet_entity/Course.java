19f1c48493f555dc2666c3b1e68a8464a2978b8b
khaled krifa <kaledkrifa1993@gmail.com>
2018-10-13 12:38:59 +0200

    -the heirachy of Source Packages has been set -the two classes Course and Location has been added to entity package -id course will incremante automaticly in the database when we add a new course

---
+++ src/main/java/lo54_projet_entity/Course.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lo54_projet_entity;
+
+/**
+ *
+ * @author kaled
+ */
+public class Course {
+    
+    private String code ; 
+    private String title ;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Course(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" + "code=" + code + ", title=" + title + '}';
+    }
+    
+    
+}
---
+++ src/main/java/lo54_projet_entity/Location.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lo54_projet_entity;
+
+/**
+ *
+ * @author kaled
+ */
+public class Location {
+    private int id ; // will be generated automaticly when we add new city to the database
+    private String city ;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+  
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Location(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "id=" + id + ", city=" + city + '}';
+    }
+    
+    
+    
+}
