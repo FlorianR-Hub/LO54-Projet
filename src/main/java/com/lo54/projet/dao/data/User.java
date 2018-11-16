cb8a35aac65430305f8c5594678fc6a409c043ce
khaled krifa <kaledkrifa1993@gmail.com>
2018-11-16 17:12:02 +0100

    Entity USER has been created

---
+++ src/main/java/com/lo54/projet/dao/data/User.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lo54.projet.dao.data;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author kaled
+ */
+@Entity
+@Table(name = "USER")
+public class User implements Serializable{
+    @Id
+    @Column(name = "USERNAME")
+    private String userName;
+    
+    @Column(name = "PASSWORD")
+    private String password;
+    
+    @Column(name = "ADMIN")
+    private boolean admin;
+
+    public User(String userName, String password, boolean admin) {
+        this.userName = userName;
+        this.password = password;
+        this.admin = admin;
+    }
+
+    public User() {
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName= userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+   @Override
+    public String toString() {
+        return "User{" + "userName=" + userName + ", password=" + password + ", admin=" + admin + '}';
+    }
+    
+    
+    
+    
+}
